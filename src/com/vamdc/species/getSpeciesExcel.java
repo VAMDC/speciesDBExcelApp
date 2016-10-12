8d2b5c88ce600c0846b54f6f5e1fdcebff2bb2a3
zwolf <zwolf@lerma-m-cz.obspm.fr>
2016-10-12 10:22:29 +0200

    Updating the URL of the species DB Rest API

--- src/com/vamdc/species/getSpeciesExcel.java
+++ src/com/vamdc/species/getSpeciesExcel.java
@@ -36,8 +36,11 @@ public class getSpeciesExcel extends HttpServlet {
 	private List<String> columnsNames;
 	private Map<String, String> columnsTypes;
 	private Map<String, List<String>> columnsDataContent;
-	private String serviceUrl;
+	private String speciesServiceUrl;
+	private String nodeServiceUrl;
 	private ExcelCreator excelCreator;
+
+	private Map<String, String> nodeIdNodeNameMap;
 
 	/**
 	 * Default constructor.
@@ -53,12 +56,12 @@ public class getSpeciesExcel extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		try {
-			serviceUrl = "http://species.vamdc.org/species_database/v1/species";
+			this.getNodeIdNodeNameMapping();
 			this.buildInternalObjectModel();
 			this.getInformationFromService();
 			this.convertIntoExcel();
 
-			DateFormat dateFormat = new SimpleDateFormat("HH-mm yyyy-MM-dd");
+			DateFormat dateFormat = new SimpleDateFormat("HH-mm-yyyy-MM-dd");
 			Date date = new Date();
 			String currentDate = dateFormat.format(date);
 
@@ -84,30 +87,52 @@ public class getSpeciesExcel extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
+	private void getNodeIdNodeNameMapping() throws IOException {
+		this.nodeIdNodeNameMap = new HashMap<String, String>();
+		nodeServiceUrl = "http://species.vamdc.org/web-service/api/v12.07/nodes";
+		URL url = new URL(nodeServiceUrl);
+
+		try (InputStream is = url.openStream();
+				JsonReader rdr = Json.createReader(is)) {
+
+			JsonArray array = rdr.readArray();
+			for (int i = 0; i < array.size(); i++) {
+				String nodeId = array.getJsonObject(i).getString(
+						"ivoIdentifier");
+				String nodeName = array.getJsonObject(i).getString("shortName");
+				this.nodeIdNodeNameMap.put(nodeId, nodeName);
+			}
+		}
+	}
+
 	private void buildInternalObjectModel() {
+		speciesServiceUrl = "http://species.vamdc.org/api/v12.07/species";
+
 		this.columnsNames = new ArrayList<String>();
-		columnsNames.add("Node identifier"); // 0
-		columnsNames.add("InChIKey"); // 1
-		columnsNames.add("name"); // 2
-		columnsNames.add("massNumber"); // 3
-		columnsNames.add("did"); // 4
-		columnsNames.add("stoichiometricFormula"); // 5
-		columnsNames.add("speciesType"); // 6
-		columnsNames.add("charge"); // 7
-		columnsNames.add("InChI"); // 8
-		columnsNames.add("formula"); // 9
+		columnsNames.add("Node Name"); // 0
+		columnsNames.add("Node identifier"); // 1
+		columnsNames.add("InChIKey"); // 2
+		columnsNames.add("name"); // 3
+		columnsNames.add("massNumber"); // 4
+		columnsNames.add("did"); // 5
+		columnsNames.add("stoichiometricFormula"); // 6
+		columnsNames.add("speciesType"); // 7
+		columnsNames.add("charge"); // 8
+		columnsNames.add("InChI"); // 9
+		columnsNames.add("formula"); // 10
 
 		this.columnsTypes = new HashMap<String, String>();
 		columnsTypes.put(columnsNames.get(0), "String");
 		columnsTypes.put(columnsNames.get(1), "String");
 		columnsTypes.put(columnsNames.get(2), "String");
-		columnsTypes.put(columnsNames.get(3), "number");
-		columnsTypes.put(columnsNames.get(4), "String");
+		columnsTypes.put(columnsNames.get(3), "String");
+		columnsTypes.put(columnsNames.get(4), "number");
 		columnsTypes.put(columnsNames.get(5), "String");
 		columnsTypes.put(columnsNames.get(6), "String");
-		columnsTypes.put(columnsNames.get(7), "number");
-		columnsTypes.put(columnsNames.get(8), "String");
+		columnsTypes.put(columnsNames.get(7), "String");
+		columnsTypes.put(columnsNames.get(8), "number");
 		columnsTypes.put(columnsNames.get(9), "String");
+		columnsTypes.put(columnsNames.get(10), "String");
 
 		this.columnsDataContent = new HashMap<String, List<String>>();
 		for (int i = 0; i < columnsNames.size(); i++) {
@@ -117,7 +142,7 @@ public class getSpeciesExcel extends HttpServlet {
 	}
 
 	private void getInformationFromService() throws IOException {
-		URL url = new URL(serviceUrl);
+		URL url = new URL(speciesServiceUrl);
 		try (InputStream is = url.openStream();
 				JsonReader rdr = Json.createReader(is)) {
 
@@ -126,9 +151,10 @@ public class getSpeciesExcel extends HttpServlet {
 				String nodeId = e.getKey();
 				JsonArray array = (JsonArray) e.getValue();
 				for (int i = 0; i < array.size(); i++) {
-					columnsDataContent.get(columnsNames.get(0)).add(nodeId);
-
-					for (int k = 1; k < columnsNames.size(); k++) {
+					columnsDataContent.get(columnsNames.get(0)).add(
+							this.nodeIdNodeNameMap.get(nodeId));
+					columnsDataContent.get(columnsNames.get(1)).add(nodeId);
+					for (int k = 2; k < columnsNames.size(); k++) {
 						String fieldName = columnsNames.get(k);
 						String fieldValue;
 						try {
@@ -152,9 +178,6 @@ public class getSpeciesExcel extends HttpServlet {
 	}
 
 	private void convertIntoExcel() {
-		DateFormat dateFormat = new SimpleDateFormat("HH-mm yyyy-MM-dd");
-		Date date = new Date();
-		String currentDate = dateFormat.format(date);
 		excelCreator = new ExcelCreator("speciesList", columnsNames,
 				columnsTypes, columnsDataContent, false);
 	}
